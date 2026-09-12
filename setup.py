141d5209e943f91d3f528a47427bd47e8068c745
harshk94138167777-png <harshk94138167777@gmail.com>
2026-09-12 14:50:40 +0530

    chore: bump version to 2.0.0 for enterprise release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="repodoctor-cli",
-    version="1.0.1",
+    version="2.0.0",
     description="Zero-dependency repository health analyser",
     long_description=long_description,
     long_description_content_type="text/markdown",
